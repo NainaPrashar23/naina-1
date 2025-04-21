1ce72f0f1d2a81d356972f08175b8ad5ed02a4d3
Naina Prashar <naina.prashar@olx.in>
2025-04-21 20:13:23 +0530

    passing props

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>App Component</h1>
+      {/* <h1>App Component</h1> */}
+      <User name = "radha" age = {21}/>
     </div>
   );
 }
--- react-redux/src/User.js
+++ react-redux/src/User.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const User = () => {
+const User = (props) => {
+    console.log(props)
     return (
         <>
            <h1>User Component</h1>
+           <p>{props.name}</p>
+           <p>{props.age}</p>
         </>
     )
 }
+
+export default User
